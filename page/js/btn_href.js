e96f8ad3cb1a8f9767f7ca14948d814945ed3e73
Zhzer <zhz961101@gmail.com>
2018-10-22 19:02:41 +0800

    docs#layout

--- page/js/btn_href.js
+++ page/js/btn_href.js
@@ -1,5 +1,8 @@
 var $ = (...args)=> document.querySelectorAll.apply(document,args);
-
+//
+// 手贱老是写错，直接写个Polyfill-lite
+// 功能简单，就是给button按钮带上href属性
+// author: zhzLuke96
 function btn_href(){
     var btns = $("button");
     for (let btn of btns) {
